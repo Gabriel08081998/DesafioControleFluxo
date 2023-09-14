b40c75127277712558f9516ab0acdda57ce554f1
gabriel <gabrieloliveira098@outlook.com>
2023-09-13 21:16:24 -0300

    Criaçao de projeto contador

---
+++ src/desafioControleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,10 @@
+package desafioControleFluxo;
+
+
+public class ParametrosInvalidosException extends Exception {
+
+	public ParametrosInvalidosException(String mensag) {
+		super (mensag);
+	}
+
+}
